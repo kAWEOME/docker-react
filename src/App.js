5623efc84731c4efd87bfe3ca63d3a2c8a7b7104
ki <ki@aviatoo.com>
2020-12-27 19:52:54 +0100

    change in feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Neuer Test
+          Neuer Test on Feature Branch
         </p>
         <a
           className="App-link"
